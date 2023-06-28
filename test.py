6ed57e1dd7f170fc13a49cfaf6fc7f1aeaa6af66
Luis Acevedo <laar@pm.me>
2023-06-28 16:57:31 -0400

    update

--- test.py
+++ test.py
@@ -2,20 +2,6 @@ import requests, json
 
 from geo.Geoserver import Geoserver
 
-geo = Geoserver("http://localhost:8080/geoserver", username="admin", password="geoserver")
-#geo = Geoserver("http://localhost:8600/geoserver", username="admin", password="geoserver")
-
-#print(help(geo))
-
-workspace = "test_workspace"
-print(geo.create_workspace(workspace=workspace))
-
-datastore = "test_datastore"
-#path      = r"/home/zurg/Desktop/code_testing/geoportal/sample_data/data/layers/Estados.shp"
-#print(geo.create_datastore(name=datastore, path=path, workspace=workspace))
-path      = r"/home/zurg/Desktop/code_testing/geoportal/sample_data/data/layers/Estados.zip"
-print(geo.create_shp_datastore(path=path, store_name="test_datastore", workspace="test_workspace"))
-
 """
 # get layer
 layer = geo.get_layer(layer_name="Estados")
@@ -25,11 +11,6 @@ print(layer)
 layers = geo.get_layers()
 print(layers)
 """
-
-
-# create feature store and publish layer
-#geo.create_featurestore(store_name="geo_data", workspace="demo", db="geoportal2", host="localhost", pg_user="postgres", pg_password="root")
-#geo.publish_featurestore(store_name="geo_data", pg_table="layers.Estado")
 
 """
 # CREATE WORKSPACE
@@ -63,3 +44,36 @@ headers = {"Content-type": "text/plain"}
 result = requests.put(url, auth=(user, password), data=data, headers=headers)
 print(result)
 """
+
+"""
+# Upload shapefile
+
+# Credentials
+geo = Geoserver("http://localhost:8080/geoserver", username="admin", password="geoserver")
+
+# Create workspace
+workspace = "test_workspace"
+print(geo.create_workspace(workspace=workspace))
+
+# Create datastore
+datastore = "test_datastore"
+path      = r"/home/zurg/Desktop/code_testing/geoportal/sample_data/data/layers/Estados.zip"
+print(geo.create_shp_datastore(path=path, store_name="test_datastore", workspace=workspace))
+
+# create feature store and publish layer
+#geo.create_featurestore(store_name="geo_data", workspace="demo", db="geoportal2", host="localhost", pg_user="postgres", pg_password="root")
+#geo.publish_featurestore(store_name="geo_data", pg_table="layers.Estado")
+"""
+
+# Upload raster
+
+# Credentials
+geo = Geoserver("http://localhost:8080/geoserver", username="admin", password="geoserver")
+
+# Create workspace
+workspace = "custom_rasters"
+print(geo.create_workspace(workspace=workspace))
+
+# For uploading raster data to the geoserver
+path      = r"/home/zurg/Desktop/code_testing/geoportal/sample_data/data/satellite_images/test.tif"
+geo.create_coveragestore(layer_name="test", path=path, workspace=workspace)
