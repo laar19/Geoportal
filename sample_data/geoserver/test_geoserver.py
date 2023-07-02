cc98f2371f03b834895a266c9d939eacc7d5cdee
Luis Acevedo <laar@pm.me>
2023-07-02 11:13:15 -0400

    update

--- sample_data/geoserver/test_geoserver.py
+++ sample_data/geoserver/test_geoserver.py
@@ -68,7 +68,7 @@ print(geo.create_shp_datastore(path=path, store_name="test_datastore", workspace
 # Upload raster
 
 # Credentials
-geo = Geoserver("http://172.25.0.3:8080/geoserver", username="admin", password="admin")
+geo = Geoserver("http://localhost:8600/geoserver", username="admin", password="admin")
 
 # Create workspace
 workspace = "custom_rasters"
