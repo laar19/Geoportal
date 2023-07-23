907b0dfaf9e7c2795d03887fd1e7528f12ac32a5
laar19 <laar@pm.me>
2023-07-23 09:49:57 -0400

    update

--- db_check.py
+++ db_check.py
@@ -1,3 +1,5 @@
+from sqlalchemy import insert
+
 from app.models.models                 import *
 from app.models.satellite_images_table import *
 from app.models.geoserver_table        import *
@@ -18,4 +20,15 @@ DbConn.check_database()
 conn, engine = DbConn.connection()
 for i in db_tables:
     check_table(engine, db_tables, i)
+
+# Add geoserver configuration
+stmt = insert(GeoserverConfig).values(
+    url         = "http://172.18.0.3:8080/geoserver",
+    workspace   = "satellite_images",
+    service     = "wms",
+    format_     = "image/png",
+    transparent = True
+)
+result = conn.execute(stmt)
+conn.commit()
 conn.close()
