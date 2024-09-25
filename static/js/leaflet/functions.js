e253da424012e35e4e39f983a8906049922418d8
Luis Acevedo <laar@pm.me>
2024-09-25 16:15:39 -0400

    update

--- static/js/leaflet/functions.js
+++ static/js/leaflet/functions.js
@@ -8,3 +8,57 @@ function toggleLayer(layerName, map, tr_id) {
         $("#"+tr_id).css("background-color", "antiquewhite");
     }
 }
+
+// Get the GetFeatureInfo URL
+function getFeatureInfoUrl(map, layer, latlng, params) {
+    var point = map.latLngToContainerPoint(latlng, map.getZoom());
+    var size  = map.getSize();
+
+    var bounds = map.getBounds();
+    var sw     = bounds.getSouthWest();
+    var ne     = bounds.getNorthEast();
+
+    var defaultParams = {
+        request     : "GetFeatureInfo",
+        service     : "WMS",
+        srs         : "EPSG:4326",
+        styles      : "",
+        transparent : true,
+        version     : "1.1.1",
+        format      : "image/png",
+        bbox        : sw.lng + "," + sw.lat + "," + ne.lng + "," + ne.lat,
+        height      : size.y,
+        width       : size.x,
+        layers      : layer.wmsParams.layers,
+        query_layers: layer.wmsParams.layers,
+        info_format : "text/html"
+    };
+
+    params = L.Util.extend(defaultParams, params);
+
+    params[params.version === "1.3.0" ? "i" : "x"] = point.x;
+    params[params.version === "1.3.0" ? "j" : "y"] = point.y;
+
+    return layer._url + L.Util.getParamString(params, layer._url, true);
+}
+
+// Parse geoserver url to strip decimals
+function parse_url(url) {
+    const urlObj = new URL(url);
+    const params = new URLSearchParams(urlObj.search);
+    const x = parseInt(params.get("X"));
+    const y = parseInt(params.get("Y"));
+    
+    params.set("X", x.toString());
+    params.set("Y", y.toString());
+    urlObj.search = params.toString();
+
+    return urlObj.toString();
+
+    /*
+    // Example usage:
+    const url = "http://localhost:8870/geoserver/wms?REQUEST=GetFeatureInfo&SERVICE=WMS&SRS=EPSG%3A4326&STYLES=&TRANSPARENT=true&VERSION=1.1.1&FORMAT=image%2Fpng&BBOX=-117.3715823598904%2C43.52560864329998%2C-117.33729299984398%2C43.54427564466141&HEIGHT=600&WIDTH=799&LAYERS=kesPolygon&QUERY_LAYERS=kesPolygon&INFO_FORMAT=application%2Fjson&X=450.09654676445825&Y=528.6470642089844";
+    const updatedURL = parse_url(url);
+    console.log(updatedURL);
+    */
+}
--- static/js/leaflet/show_raster_info.js
+++ static/js/leaflet/show_raster_info.js
@@ -2,7 +2,75 @@ function show_raster_info(map, geoserver_config, layers, error) {
     var map_layers = {};
 
     //var aux = 0;
+    layers_ = [];
     for(let key in layers) {
+        layers_.push(key);
+    }
+    console.log(layers_);
+
+    /*
+    var test = L.tileLayer.wms("http://172.20.0.3:8080/geoserver/satellite_images/wms", {
+        layers     : "satellite_images:VRSS-2_MSS_0288_0330_20221022_L2A_404132008447_THUMB_modified",
+        format     : "image/png",
+        transparent: true,
+        //version    : '1.1.0',
+        //attribution: "country layer"
+    });
+    test.addTo(map);
+    */
+
+    var geoserver_url = geoserver_config["geoserver_url"] + "/" +
+        //layers[key]["geoserver_workspace"]  + "/" +
+        "wms";
+    
+    var wmsLayer = L.tileLayer.wms(geoserver_url, {
+        //layers     : layers[key]["geoserver_workspace"] + ":" + layers[key]["custom_id"],
+        layers     : layers_,
+        format     : "image/png",
+        transparent: true,
+        //name       : _id
+        //version    : '1.1.0',
+        //attribution: "country layer"
+    });
+    wmsLayer.addTo(map);
+
+    // Add a click event to show popup
+    map.on("click", function(e) {
+        var url = getFeatureInfoUrl(
+            map,
+            wmsLayer,
+            e.latlng,
+            {
+                "info_format": "application/json"
+            }
+        );
+
+        parsed_url = parse_url(url);
+        fetch(parsed_url)
+            .then(response => response.json())
+            .then(data => {
+                var feature = data.features[0];
+                if (feature !== undefined) {
+                    //var popupContent  = "<p>" + feature.properties.your_property + "</p>";
+                    var popupContent  = "<p>" + feature.properties + "</p>";
+                    L.popup()
+                    .setLatLng(e.latlng)
+                    .setContent(popupContent)
+                    .openOn(map);
+                }
+            });
+    });
+
+    //$("#previews").append('<div id='+id_+'></div>');
+    //$("#informacion").append('<div id='+id_+'></div>');
+    $("#"+layers[key]["custom_id"]+"").click(function() {
+        var layer = map_layers[this.id];
+        toggleLayer(layer, map, "div_"+layers[key]["custom_id"]);
+    });
+
+    /*
+    for(let key in layers) {
+        console.log(key);
         /*
         var test = L.tileLayer.wms("http://172.20.0.3:8080/geoserver/satellite_images/wms", {
             layers     : "satellite_images:VRSS-2_MSS_0288_0330_20221022_L2A_404132008447_THUMB_modified",
@@ -12,9 +80,11 @@ function show_raster_info(map, geoserver_config, layers, error) {
             //attribution: "country layer"
         });
         test.addTo(map);
-        */
+        *
 
-        var geoserver_url = geoserver_config["geoserver_url"] + layers[key]["geoserver_workspace"]  + "/" + layers[key]["geoserver_service"];
+        var geoserver_url = geoserver_config["geoserver_url"] + "/" +
+            //layers[key]["geoserver_workspace"]  + "/" +
+            layers[key]["geoserver_service"];
         
         var wmsLayer = L.tileLayer.wms(geoserver_url, {
             //layers     : layers[key]["geoserver_workspace"] + ":" + layers[key]["custom_id"],
@@ -25,142 +95,39 @@ function show_raster_info(map, geoserver_config, layers, error) {
             //version    : '1.1.0',
             //attribution: "country layer"
         });
-        //wmsLayer.addTo(map);
         map_layers[layers[key]["custom_id"]] = wmsLayer;
 
         if (layers[key]["layer_type"] == "vector") {
-            wmsLayer.addTo(map).bindPopup("My popup content", {
-                trigger: 'hover',
-                maxWidth: 200,
-                className: 'custom-popup'
-            });
+            wmsLayer.addTo(map);
 
-            /*
-            // Function to retrieve polygon data from WFS
-            function getPolygonData(wmsUrl, layerName) {
-                var wfsUrl = wmsUrl + '?service=WFS&version=1.1.0&request=GetFeature&typeName=' + layerName + '&outputFormat=application/json';
+            //.bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup();
 
-                axios.get(wfsUrl)
-                    .then(function (response) {
-                        var features = response.data.features;
-                        for (var i = 0; i < features.length; i++) {
-                            var feature = features[i];
-                            var coordinates = feature.geometry.coordinates;
-                            var polygon = L.polygon(coordinates).addTo(map).bindPopup("hi jeje");
-                            console.log(coordinates);
-                        }
-                    })
-                    .catch(function (error) {
-                        console.error('Error fetching polygon data:', error);
-                    });
-            }
-
-            // Call the function to retrieve polygon data
-            getPolygonData(geoserver_config["geoserver_url"]+"wms", layers[key]["custom_id"]);
-            */
-
-            /*
-            // Add click event listener
-            map.on('click', function(e) {
-                var url = wmsLayer.getFeatureInfoUrl(
+            // Add a click event to show popup
+            map.on("click", function(e) {
+                var url = getFeatureInfoUrl(
+                    map,
+                    wmsLayer,
                     e.latlng,
-                    map.getZoom(),
-                    map.getSize(),
                     {
-                        'info_format': 'application/json'
+                        "info_format": "application/json"
                     }
                 );
 
-                // Fetch the feature info
-                fetch(url)
+                parsed_url = parse_url(url);
+                fetch(parsed_url)
                     .then(response => response.json())
                     .then(data => {
-                        // Handle the feature info data
-                        console.log(data);
-                        L.popup()
+                        var feature = data.features[0];
+                        if (feature !== undefined) {
+                            //var popupContent  = "<p>" + feature.properties.your_property + "</p>";
+                            var popupContent  = "<p>" + feature.properties + "</p>";
+                            L.popup()
                             .setLatLng(e.latlng)
-                            .setContent('Feature Info: ' + JSON.stringify(data))
+                            .setContent(popupContent)
                             .openOn(map);
-                    })
-                    .catch(error => console.error('Error fetching feature info:', error));
+                        }
+                    });
             });
-            */
-
-            /*
-            map.on('click', function (e) {
-                const url = geoserver_config["geoserver_url"]+"wms?service=WMS&version=1.1.1&request=GetFeatureInfo&layers="+layers[key]["geoserver_workspace"]+":"+layers[key]["custom_id"]+"&bbox=${map.getBounds().toBBoxString()}&width=${map.getSize().x}&height=${map.getSize().y}&srs=EPSG:4326&format=image/png&query_layers="+layers[key]["geoserver_workspace"]+":"+layers[key]["custom_id"]+"&info_format=application/json&x=${Math.floor(e.containerPoint.x)}&y=${Math.floor(e.containerPoint.y)}";
-
-                fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log('Feature Info:', data);
-                        // Display the feature info as needed
-                        alert(JSON.stringify(data));
-                    })
-                    .catch(error => console.error('Error fetching feature info:', error));
-            });
-            */
-
-            /*
-            // Get the bounds of the geoserver WMS layer to draw
-            const wmsUrl    = geoserver_config["geoserver_url"] + "wms?service=WMS&version=1.1.1&request=GetCapabilities";
-            const workspace = layers[key]["geoserver_workspace"];
-            const layerName = layers[key]["custom_id"];
-
-            var coordinates_vector;
-
-            axios.get(wmsUrl).then((response) => {
-                const parser = new WMSCapabilities();
-                const json   = parser.parse(response.data);
-                const layers = json.Capability.Layer.Layer;
-                const layer  = layers.find(l => l.Name === `${workspace}:${layerName}`);
-                const bbox   = layer.BoundingBox[0].extent;
-
-                var coordinates = [[]];
-                for (var i=0; i<bbox.length; i++) {
-                    coordinates[0].push([bbox[i][1], bbox[i][0]]);
-                }
-                coordinates_vector = coordinates;
-
-                console.log("Bounding Box:", bbox);
-                // Use the bounding box as needed, e.g., to fit the map view
-                // map.fitBounds([[bbox[1], bbox[0]], [bbox[3], bbox[2]]]);
-            }).catch((error) => {
-                console.error("Error fetching WMS capabilities:", error);
-            });
-
-            console.log(coordinates_vector);
-
-            var polygon  = L.polygon(coordinates_vector, polygon_style);
-            var polyline = L.polyline(coordinates_vector, {color: "red"});
-            polygon.addTo(map);
-            polyline.addTo(map);
-
-            var html_layer_info = "<div>"+
-                "<table>"+
-                    "<tr>"+
-                        "<td>Hello</td>"
-                "</table>"+
-            "</div>";
-
-            wmsLayer.bindPopup(html_layer_info);
-
-            function show_popup(e) {
-                var popup = e.target.getPopup();
-
-                $.ajax({
-                    //url: "myurl.html",
-                    url: $(location).attr("host"),
-                })
-                .done(function(data) {
-                    alert(data);
-                    popup.setContent(data);
-                    popup.update();
-                })
-            };
-
-            wmsLayer.on("click", show_popup);
-            */
         }
 
         //$("#previews").append('<div id='+id_+'></div>');
@@ -230,76 +197,8 @@ function show_raster_info(map, geoserver_config, layers, error) {
                 polyline.addTo(map);
 
                 polygon.bindPopup(html_layer_info);
-
-                /*
-                function show_popup(e) {
-                    var popup = e.target.getPopup();
-
-                    $.ajax({
-                        //url: "myurl.html",
-                        url: $(location).attr("host"),
-                    })
-                    .done(function(data) {
-                        alert(data);
-                        popup.setContent(data);
-                        popup.update();
-                    })
-                };
-
-                polygon.on("click", show_popup);
-                */
             }
         }
     }
-
-    /*
-    const polygon2 = L.polygon([
-        [472545.74484795, 4823134.54431552],
-        [472543.90161885, 4823132.13071817],
-        [472523.76152767, 4823124.8830602]
-    ]).addTo(map).bindPopup('I am a polygon.');
-    */
-
-    /*
-    // Define the source projection using its Proj4 definition string
-    proj4.defs("EPSG:32611", "+proj=utm +zone=11 +datum=WGS84 +units=m +no_defs");
-
-    // Define the source and destination projections
-    var sourceProjection = 'EPSG:32611'; // UTM Zone 33N
-    var destProjection = 'EPSG:4326'; // WGS84
-
-    // Example projected coordinates (easting, northing)
-    var projectedCoordinates = [
-        [472545.74484795, 4823134.54431552],
-        [472543.90161885, 4823132.13071817],
-        [472523.76152767, 4823124.8830602]
-    ];
-    //var projectedCoordinates = [472545.74484795, 4823134.54431552];
-
-    // Perform the conversion
-    var geographicCoordinates = proj4(sourceProjection, destProjection, projectedCoordinates);
-
-    console.log('Latitude:', geographicCoordinates[1]);
-    console.log('Longitude:', geographicCoordinates[0]);
-    */
-
-    /*
-    //var proj4 = require('proj4');
-
-    // Define the source and target projections
-    var fromProj = 'EPSG:3857'; // Example: Web Mercator
-    var toProj = 'EPSG:4326'; // Example: WGS84 (geographic)
-
-    // Projected coordinates
-    var projectedCoordinates = [
-        [472545.74484795, 4823134.54431552],
-        [472543.90161885, 4823132.13071817],
-        [472523.76152767, 4823124.8830602]
-    ];
-
-    // Convert to geographic coordinates
-    var geographicCoords = proj4(fromProj, toProj, projectedCoordinates);
-
-    console.log(geographicCoords); // Output: [longitude, latitude]
     */
 }
