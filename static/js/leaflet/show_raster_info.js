a103869968086c9981580d30926dd4cff88e2eb7
Luis Acevedo <laar@pm.me>
2024-09-25 22:36:47 -0400

    update

--- static/js/leaflet/show_raster_info.js
+++ static/js/leaflet/show_raster_info.js
@@ -1,76 +1,8 @@
 function show_raster_info(map, geoserver_config, layers, error) {
-    var map_layers = {};
+    var map_layers  = [];
+    var map_layers_ = {};
 
-    //var aux = 0;
-    layers_ = [];
     for(let key in layers) {
-        layers_.push(key);
-    }
-    console.log(layers_);
-
-    /*
-    var test = L.tileLayer.wms("http://172.20.0.3:8080/geoserver/satellite_images/wms", {
-        layers     : "satellite_images:VRSS-2_MSS_0288_0330_20221022_L2A_404132008447_THUMB_modified",
-        format     : "image/png",
-        transparent: true,
-        //version    : '1.1.0',
-        //attribution: "country layer"
-    });
-    test.addTo(map);
-    */
-
-    var geoserver_url = geoserver_config["geoserver_url"] + "/" +
-        //layers[key]["geoserver_workspace"]  + "/" +
-        "wms";
-    
-    var wmsLayer = L.tileLayer.wms(geoserver_url, {
-        //layers     : layers[key]["geoserver_workspace"] + ":" + layers[key]["custom_id"],
-        layers     : layers_,
-        format     : "image/png",
-        transparent: true,
-        //name       : _id
-        //version    : '1.1.0',
-        //attribution: "country layer"
-    });
-    wmsLayer.addTo(map);
-
-    // Add a click event to show popup
-    map.on("click", function(e) {
-        var url = getFeatureInfoUrl(
-            map,
-            wmsLayer,
-            e.latlng,
-            {
-                "info_format": "application/json"
-            }
-        );
-
-        parsed_url = parse_url(url);
-        fetch(parsed_url)
-            .then(response => response.json())
-            .then(data => {
-                var feature = data.features[0];
-                if (feature !== undefined) {
-                    //var popupContent  = "<p>" + feature.properties.your_property + "</p>";
-                    var popupContent  = "<p>" + feature.properties + "</p>";
-                    L.popup()
-                    .setLatLng(e.latlng)
-                    .setContent(popupContent)
-                    .openOn(map);
-                }
-            });
-    });
-
-    //$("#previews").append('<div id='+id_+'></div>');
-    //$("#informacion").append('<div id='+id_+'></div>');
-    $("#"+layers[key]["custom_id"]+"").click(function() {
-        var layer = map_layers[this.id];
-        toggleLayer(layer, map, "div_"+layers[key]["custom_id"]);
-    });
-
-    /*
-    for(let key in layers) {
-        console.log(key);
         /*
         var test = L.tileLayer.wms("http://172.20.0.3:8080/geoserver/satellite_images/wms", {
             layers     : "satellite_images:VRSS-2_MSS_0288_0330_20221022_L2A_404132008447_THUMB_modified",
@@ -80,33 +12,40 @@ function show_raster_info(map, geoserver_config, layers, error) {
             //attribution: "country layer"
         });
         test.addTo(map);
-        *
+        */
 
         var geoserver_url = geoserver_config["geoserver_url"] + "/" +
-            //layers[key]["geoserver_workspace"]  + "/" +
+            //map_layers[key].layer_data["geoserver_workspace"]  + "/" +
             layers[key]["geoserver_service"];
         
         var wmsLayer = L.tileLayer.wms(geoserver_url, {
-            //layers     : layers[key]["geoserver_workspace"] + ":" + layers[key]["custom_id"],
+            //layers     : map_layers[key].layer_data["geoserver_workspace"] + ":" + map_layers[key].layer_data["custom_id"],
             layers     : layers[key]["custom_id"],
             format     : layers[key]["geoserver_format"],
             transparent: layers[key]["geoserver_transparent"],
             //name       : _id
             //version    : '1.1.0',
             //attribution: "country layer"
-        });
-        map_layers[layers[key]["custom_id"]] = wmsLayer;
+        }).addTo(map);
 
-        if (layers[key]["layer_type"] == "vector") {
-            wmsLayer.addTo(map);
+        var tmp = {
+            layer_object: wmsLayer,
+            layer_data  : layers[key]
+            
+        };
+        map_layers.push(tmp);
+        map_layers_[layers[key]["custom_id"]] = wmsLayer;
+        
+    }
 
-            //.bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup();
-
+    for(let key in map_layers) {
+        if (map_layers[key].layer_data["layer_type"] == "vector") {
+            
             // Add a click event to show popup
             map.on("click", function(e) {
                 var url = getFeatureInfoUrl(
                     map,
-                    wmsLayer,
+                    map_layers[key].layer_object,
                     e.latlng,
                     {
                         "info_format": "application/json"
@@ -119,64 +58,90 @@ function show_raster_info(map, geoserver_config, layers, error) {
                     .then(data => {
                         var feature = data.features[0];
                         if (feature !== undefined) {
-                            //var popupContent  = "<p>" + feature.properties.your_property + "</p>";
-                            var popupContent  = "<p>" + feature.properties + "</p>";
+                            
+                            var popupContent  = "<p>" +
+                                    map_layers[key].layer_data["custom_id"] +
+                                "</p>";
+                                
                             L.popup()
-                            .setLatLng(e.latlng)
-                            .setContent(popupContent)
-                            .openOn(map);
+                                .setLatLng(e.latlng)
+                                .setContent(popupContent)
+                                .openOn(map);
                         }
                     });
             });
         }
 
-        //$("#previews").append('<div id='+id_+'></div>');
-        //$("#informacion").append('<div id='+id_+'></div>');
-        $("#"+layers[key]["custom_id"]+"").click(function() {
-            var layer = map_layers[this.id];
-            toggleLayer(layer, map, "div_"+layers[key]["custom_id"]);
+        $("#"+map_layers[key].layer_data["custom_id"]+"").click(function() {
+            var layer = map_layers_[this.id];
+            
+            toggleLayer(
+                layer,
+                map,
+                "div_"+map_layers[key].layer_data["custom_id"]
+            );
         });
 
-        if (layers[key]["layer_type"] == "raster") {
-            var html_layer_info = "<div>"+
-                "<table>"+
-                    "<tr>"+
-                        "<td>Satellite</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["satellite"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Sensor</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["sensor"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Orbit</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["orbit"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Scene</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["scene"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Capture date</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["capture_date"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Cloud percentage</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["cloud_percentage"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Roll angle</td>"+
-                        "<td class='popup-table-value'>" + layers[key]["roll_angle"] + "</td>"+
-                    "</tr>"+
-                    "<tr>"+
-                        "<td>Download</td>"+
-                        "<td class='popup-table-value'>" + "<a href="+layers[key]["compressed_file_path"]+">Link</a>"+ "</td>"+
-                    "</tr>"+
-                "</table>"+
-            "</div>";
+        if (map_layers[key].layer_data["layer_type"] == "raster") {
+            var html_layer_info = "<div>" +
+                    "<table>" +
+                        "<tr>" +
+                            "<td>Satellite</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["satellite"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Sensor</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["sensor"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Orbit</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["orbit"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Scene</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["scene"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Capture date</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["capture_date"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Cloud percentage</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["cloud_percentage"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Roll angle</td>" +
+                            "<td class='popup-table-value'>" +
+                                map_layers[key].layer_data["roll_angle"] +
+                            "</td>" +
+                        "</tr>" +
+                        "<tr>" +
+                            "<td>Download</td>" +
+                            "<td class='popup-table-value'>" +
+                                "<a href=" +
+                                    map_layers[key]
+                                        .layer_data["compressed_file_path"] +
+                                ">Link</a>" +
+                            "</td>" +
+                        "</tr>" +
+                    "</table>" +
+                "</div>";
             
-            if (layers[key]["cutted_image_shape"] != "None") {
-                var coords = JSON.parse(layers[key]["cutted_image_shape"])["coordinates"];
+            if (map_layers[key].layer_data["cutted_image_shape"] != "None") {
+                var coords = JSON.parse(map_layers[key]
+                    .layer_data["cutted_image_shape"])["coordinates"];
                 //var coords =  [[48,-3],[50,5],[44,11],[48,-3]] ;
                 
                 var coordinates = [[]];
@@ -200,5 +165,4 @@ function show_raster_info(map, geoserver_config, layers, error) {
             }
         }
     }
-    */
 }
